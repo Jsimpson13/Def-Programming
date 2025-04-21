9601cc5e7c7f2da7274a4bc66cc43069732506cb
ddogg26 <68987840+ddogg26@users.noreply.github.com>
2025-04-21 17:00:03 -0400

    Should be ready to merge into main

--- start-program.sh
+++ start-program.sh
@@ -7,4 +7,4 @@ gnome-terminal -- bash -c "cd ./Server && python3.12 serverMain.py; exec bash"
 sleep 2
 
 # Terminal 2: Run client.py in Def-Programming/Client/Backend
-gnome-terminal -- bash -c "cd ./Client/Backend && python3.12 client.py; exec bash"
+gnome-terminal -- bash -c "cd ./Client && python3.12 client.py; exec bash"
